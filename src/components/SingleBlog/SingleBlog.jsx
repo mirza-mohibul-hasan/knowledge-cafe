f9050351c81dc19c3d1dc7193a587b81eaf978ab
Mirza Mohibul Hasan <mirzamohibul618@gmail.com>
2023-04-01 15:51:02 +0600

    Horizontal line added

--- src/components/SingleBlog/SingleBlog.jsx
+++ src/components/SingleBlog/SingleBlog.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './Singleblog.css'
 import bookmarkimg from '../../assets/bookmark.svg'
 const Singleblog = (props) => {
-    // console.log(props)
     const { id, authorname, title, cover, img, publishdate, readtime } = props.blog;
     const handleTimeSpent = props.handleTimeSpent;
     const handleBookmark = props.handleBookmark;
@@ -18,15 +17,20 @@ const Singleblog = (props) => {
                     </div>
                 </div>
                 <div className='read-bm-container'>
-                    <p>{readtime} min read <button onClick={()=>handleBookmark(props.blog)}><img src={bookmarkimg} alt="" /></button></p>
+                    <p>{readtime} min read <button className='bookmark-btn' onClick={()=>handleBookmark(props.blog)}><img src={bookmarkimg} alt="" /></button></p>
                 </div>
             </div>
             <div className='singleblog-last'>
                 <h1 className='blog-title'>{title}</h1>
                 <button onClick={()=>handleTimeSpent(readtime)} className='mark-btn'>Mark as read</button>
+                {id<4?<Border></Border>:''}
             </div>
         </div>
     );
 };
-
+function Border(){
+    return(
+        <hr style={{marginBottom:"20px"}} />
+    )
+}
 export default Singleblog;
